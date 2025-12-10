f2ef43475275d0be8affc151e0737fd33987e8ef
Gordon Li <gordon.li@duke.edu>
2025-12-10 08:40:20 -0500

    build static site hopefully final

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/face-ui_static/",   
-});
+  base: '/face-ui-static/',   // repo name on GitHub
+  build: {
+    outDir: 'docs',           // GitHub Pages will serve from /docs
+  },
+})
